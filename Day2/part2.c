44e40698fba25ff141058b09fac1a1b5612ead53
Khristian Jones <jones.khristian26@gmail.com>
2021-12-26 17:59:05 -0700

    Added Day 2 Part 2 code

---
+++ Day2/part2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    FILE *fp;
+    char buff[255];
+    fp = fopen("input.txt", "r"); //Opens input file
+    int fileLine = 0;
+    int horizontal=0;
+    int depth=0;
+    int aim=0;
+
+
+     while (!feof(fp)){
+     //Reads until End of Line
+            fscanf(fp, "%s", buff); //Scans from stream and put into buffer
+            if(!strcmp(buff,"down")){
+                //printf("DOWN");
+                fscanf(fp, "%s",buff);
+                aim+= atoi(buff);
+            }
+            else if(!strcmp(buff,"up")){
+                //printf("UP");
+                fscanf(fp, "%s",buff);
+                aim-= atoi(buff);
+            }
+            else if(!strcmp(buff,"forward")){
+                //printf("Forward");
+                fscanf(fp, "%s",buff);
+                horizontal+= atoi(buff);
+                depth+=aim*atoi(buff);
+            }
+            printf("%d: %s\n", fileLine, buff); //Print buffer
+            fileLine++;
+            }
+    fclose(fp);
+    printf("Horizontal: %d\n",horizontal);
+    printf("Depth: %d\n",depth);
+    printf("Aim: %d\n",aim);
+    printf("Product of Horizontal position * Depth: %d\n",horizontal*depth);
+    printf("End of Program\n");
+}
